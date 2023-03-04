2e8cb3544d700bbbc60455ce473c1684f31222a3
ethan <ethan.gonzalez00774@gmail.com>
2023-03-04 02:01:27 -0800

    third challenge, bool datatype

---
+++ third-challenge-numuric.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main () {
+
+    float example1 = 22.1;
+    double example2 = 123456711111111111.2;
+    double example3 = 55.5555555e+11;//with scientific notation, needs to be together no spaces.
+
+    _Bool boolVariable = 1; //with _Bool type, not commonly used
+    bool boolVariable2 = true; //C99 standard, instead of saying 1/0 you could use true/false. commonly used
+
+
+
+
+
+
+
+
+
+}
